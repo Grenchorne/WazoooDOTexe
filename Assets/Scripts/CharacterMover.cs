b468b3b04ca20580fb2dab2a3efbeb07395ed2c2
Mike Guthrie-Plouffe <michaeljplouffe@gmail.com>
2020-10-24 11:12:53 -0600

    Initial project creation, plugins
    LeanTween, OdinInspector - see manifest for packages

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Adhaesii.WazoooDOTexe
+{
+    public class CharacterMover : SerializedMonoBehaviour
+    {
+        [SerializeField] private float moveSpeed;
+
+        public void Move(float move)
+        {
+            
+        }
+    }
+}
