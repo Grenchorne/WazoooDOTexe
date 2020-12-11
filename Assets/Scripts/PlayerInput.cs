6bc17f772651f0983186138014c0fe8124854b21
Mike Guthrie-Plouffe <michael.guthrie.plouffe@gmail.com>
2020-12-10 17:08:57 -0700

    Added some fonts, created menu
    super simple menu to just show the controls

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -26,14 +27,22 @@ namespace Adhaesii.WazoooDOTexe
         public bool Hover { get; private set; }
         
         public float Peek { get; private set; }
+        
+        public bool NoInput { get; private set; }
+        public float TimeSinceLastInput { get; private set; }
 
         private void Update()
         {
             Horizontal = Input.GetAxisRaw(horizontalAxis);
             Jump = Input.GetAxis(jumpAxis) > 0;
             Attack = Input.GetAxis(attackAxis) > 0;
+            //Attack = Input.GetMouseButtonDown(0);
             Hover = Input.GetAxis(hoverAxis) > 0;
             Peek = Input.GetAxis(peekAxis);
+
+            NoInput = Mathf.Approximately(Horizontal, 0) && !Jump && !Attack && !Hover && Mathf.Approximately(Peek, 0);
+
+            TimeSinceLastInput = NoInput ? TimeSinceLastInput + Time.deltaTime : 0f; 
         }
     }
 }
---
+++ Assets/Scripts/UI/MainUIController.cs
@@ -0,0 +1,67 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Adhaesii.WazoooDOTexe.UI
+{
+    public class MainUIController : SerializedMonoBehaviour
+    {
+        [SerializeField, Required]
+        private PlayerInput playerInput;
+
+        [SerializeField]
+        private float timeout = 15f;
+
+        [SerializeField, Required]
+        private CanvasGroup mainMenu;
+
+        [SerializeField, Required]
+        private CanvasGroup gameplayMenu;
+
+        enum Mode
+        {
+            Title,
+            Gameplay
+        }
+
+        private Mode mode = Mode.Title;
+
+        private void Start() => ChangeMode(mode);
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                ChangeMode(mode == Mode.Title ? Mode.Gameplay : Mode.Title);
+            return;
+            switch (mode)
+            {
+                case Mode.Title when !playerInput.NoInput:
+                    ChangeMode(Mode.Gameplay);
+                    break;
+                case Mode.Gameplay when playerInput.TimeSinceLastInput > timeout:
+                    ChangeMode(Mode.Title);
+                    break;
+            }
+        }
+
+        private void ChangeMode(Mode mode)
+        {
+            switch (mode)
+            {
+                case Mode.Title:
+                    this.mode = Mode.Title;
+                    LeanTween.alphaCanvas(mainMenu, 1, 1f);
+                    LeanTween.alphaCanvas(gameplayMenu, 0, 1f);
+                    break;
+                case Mode.Gameplay:
+                    this.mode = Mode.Gameplay;
+                    LeanTween.alphaCanvas(mainMenu, 0, 1f);
+                    LeanTween.alphaCanvas(gameplayMenu, 1, 1f);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+        }
+        
+    }
+}
