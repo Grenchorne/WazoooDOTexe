1c808e9125946ba84d81927507148da1ab0f1b1b
Mike Guthrie-Plouffe <michael.guthrie.plouffe@gmail.com>
2020-12-15 18:07:40 -0700

    kill trigger now damages player

--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -1,3 +1,4 @@
+using Adhaesii.WazoooDOTexe.Audio;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -10,18 +11,24 @@ namespace Adhaesii.WazoooDOTexe
 
         [SerializeField] private string playerTag = "Player";
 
+        [SerializeField]
+        private int damage = 1;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (_layerMask != (_layerMask | 1 << other.gameObject.layer)) return;
             
             if (other.CompareTag(playerTag))
             {
+                if(other.TryGetComponent(out HealthController health))
+                    health.Damage(gameObject);
+                
                 if (other.TryGetComponent(out PlayerRespawnHandler respawnHandler))
                     respawnHandler.Respawn();
                 
                 else
                 {
-                    other.transform.position = Vector3.zero;        
+                    other.transform.position = Vector3.zero;
                     if(other.TryGetComponent(out Rigidbody2D rb))
                         rb.velocity = Vector2.zero;
                 }
